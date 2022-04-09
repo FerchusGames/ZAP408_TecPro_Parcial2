d8b65a3f9688f827b76974fad549db420daa4fea
Ferchus <ferchusgames@protonmail.com>
2022-04-08 20:49:41 -0500

    Entrega Final V1

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <string>
+#include <locale> // Para cambiar el idioma
+#include "Text.h" // Aquí tengo todos los texos que necesito para la historia
+#include <queue> // Voy a usar un queue para guardar las decisiones que va tomando el jugador
+#include <Windows.h>
+#include <conio.h>
 
-	class Texto // Clase base para cualquiera de los textos
+	// Clase base para cualquiera de los textos
+
+	class Texto 
 	{
 	public:
 		Texto(std::string _texto);
-		virtual void MostrarTexto();
+		virtual int MostrarTexto();
 	private:
 		std::string texto;
 	};
@@ -15,46 +22,410 @@
 		texto = _texto;
 	}
 
-	void Texto::MostrarTexto()
+	int Texto::MostrarTexto()
 	{
+		std::system("CLS");
 		std::cout << texto << std::endl << std::endl;
+		return 0;
 	}
 
-	class Decision
+	// Clase para los textos de interacción
+
+	class Interaccion : public Texto
 	{
 	public:
-		Decision();
+		Interaccion(std::string _texto);
+		int MostrarTexto();
 
 	private:
-		std::string texto;
-		std::string opcion1;
-		std::string opcion2;
 	};
 
-	class Consecuencia
+	Interaccion::Interaccion(std::string _texto) : Texto(_texto){}
+
+	int Interaccion::MostrarTexto()
 	{
-	public:
-		Consecuencia();
+		Texto::MostrarTexto();
 
-	private:
+		int respuesta;
 
-	};
+		std::cin >> respuesta;
 
-	Consecuencia::Consecuencia()
-	{
+		return respuesta;
 	}
 
-
-	Decision::Decision()
+	int CrearMostrarQueueInteraccion(Interaccion* &interaccionActual, std::string texto, std::queue<int> &respuestas)
 	{
+		int respuestaActual;
+		interaccionActual = new Interaccion(texto);
+		respuestaActual = interaccionActual->MostrarTexto(); 
+		respuestas.push(respuestaActual); // Agrega la respuesta actual al queue
 
+		return respuestaActual;
+
+	}
+
+	
+	// Clase para los textos de consecuencias
+
+	class Consecuencia : public Texto
+	{
+	public:
+		Consecuencia(std::string _texto);
+		int MostrarTexto();
+
+	private:
+	};
+
+	Consecuencia::Consecuencia(std::string _texto) : Texto(_texto){}
+
+	int Consecuencia::MostrarTexto()
+	{
+		Texto::MostrarTexto();
+
+		_getch();
+
+		return 0;
+	}
+
+	void CrearMostrarConsecuencia(Consecuencia* &consecuenciaActual, std::string texto) 
+	{
+		consecuenciaActual = new Consecuencia(texto);
+		consecuenciaActual->MostrarTexto();
 	}
 
 int main()
 {
-	bool enProgreso;	// La historia no se ha acabado
-	bool estaVivo;		// El personaje del jugador sigue con vida 
+	setlocale(LC_ALL, "spanish");
 
-	Texto test("Hola");
-	test.MostrarTexto();
+	std::queue<int> respuestas; // Aquí guardaré todas las respuestas numéricas de las interacciones (Las decisiones en la historia)
+
+	Interaccion* interaccionActual = NULL; // Para crear instancias de Interaccion
+	Consecuencia* consecuenciaActual = NULL; // Para crear instancias de Consecuencia
+
+	int vida = 100; 
+
+	
+	// Interacción 1
+	std::string nombreJugador;
+	std::cout << interaccion1 << std::endl << std::endl;
+	std::getline(std::cin, nombreJugador);
+
+	std::system("CLS");
+	std::cout << "Muy bien.¡Suerte en tu aventura, " << nombreJugador;
+	std::cin.get();
+
+	// Interacción 2
+	int respuestaActual = CrearMostrarQueueInteraccion(interaccionActual, interaccion2, respuestas);
+
+	if (respuestaActual == 1)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia1_2);
+			goto end; // Aquí el usuario llegó a un final
+	}
+
+	if (respuestaActual == 2)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia2_2);
+	}
+	
+	if (vida <= 0) goto end;
+
+	// Interacción 3
+	respuestaActual = CrearMostrarQueueInteraccion(interaccionActual, interaccion3, respuestas);
+
+	if (respuestaActual == 1)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia1_3);
+	}
+
+	if (respuestaActual == 2)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia2_3);
+		goto end; // Aquí el usuario llegó a un final
+	}
+	
+	if (vida <= 0) goto end;
+
+	// Interacción 4
+	respuestaActual = CrearMostrarQueueInteraccion(interaccionActual, interaccion4, respuestas);
+
+	if (respuestaActual == 1)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia1_4);
+	}
+
+	if (respuestaActual == 2)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia2_4);
+		vida = vida - 20;
+	}
+
+	if (vida <= 0) goto end;
+
+	// Interacción 5
+	respuestaActual = CrearMostrarQueueInteraccion(interaccionActual, interaccion5, respuestas);
+
+	if (respuestaActual == 1)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia1_5);
+		vida = vida - 30;
+	}
+
+	if (respuestaActual == 2)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia2_5);
+	}
+
+	if (vida <= 0) goto end;
+
+	// Interacción 6
+	respuestaActual = CrearMostrarQueueInteraccion(interaccionActual, interaccion6, respuestas);
+
+	if (respuestaActual == 1)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia1_6);
+		vida = vida - 30;
+	}
+
+	if (respuestaActual == 2)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia2_6);
+	}
+
+	if (vida <= 0) goto end;
+
+	// Interacción 7
+	respuestaActual = CrearMostrarQueueInteraccion(interaccionActual, interaccion7, respuestas);
+
+	if (respuestaActual == 1)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia1_7);
+		vida = vida - 10;
+	}
+
+	if (respuestaActual == 2)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia2_7);
+	}
+
+	if (vida <= 0) goto end;
+
+	// Interacción 8
+	respuestaActual = CrearMostrarQueueInteraccion(interaccionActual, interaccion8, respuestas);
+
+	if (respuestaActual == 1)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia1_8);
+	}
+
+	if (respuestaActual == 2)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia2_8);
+		goto end;
+	}
+
+	if (vida <= 0) goto end;
+
+	// Interacción 9
+	respuestaActual = CrearMostrarQueueInteraccion(interaccionActual, interaccion9, respuestas);
+
+	if (respuestaActual == 1)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia1_9);
+		vida = vida - 40;
+	}
+
+	if (respuestaActual == 2)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia2_9);
+	}
+
+	if (vida <= 0) goto end;
+
+	// Interacción 10
+	respuestaActual = CrearMostrarQueueInteraccion(interaccionActual, interaccion10, respuestas);
+
+	if (respuestaActual == 1)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia1_10);
+	}
+
+	if (respuestaActual == 2)
+	{
+		goto torito;
+	}
+
+	if (vida <= 0) goto end;
+
+	// Interacción 12
+	respuestaActual = CrearMostrarQueueInteraccion(interaccionActual, interaccion12, respuestas);
+
+	if (respuestaActual == 1)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia1_12);
+	}
+
+	if (respuestaActual == 2)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia2_12);
+		goto end;
+	}
+
+	if (vida <= 0) goto end;
+
+	// Interacción 13
+	respuestaActual = CrearMostrarQueueInteraccion(interaccionActual, interaccion13, respuestas);
+
+	if (respuestaActual == 1)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia1_13);
+	}
+
+	if (respuestaActual == 2)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia2_13);
+		vida = vida - 20;
+	}
+
+	if (vida <= 0) goto end;
+
+	// Interacción 14
+	respuestaActual = CrearMostrarQueueInteraccion(interaccionActual, interaccion14, respuestas);
+
+	if (respuestaActual == 1)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia1_14);
+		goto end;
+	}
+
+	if (respuestaActual == 2)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia2_14);
+	}
+
+	if (vida <= 0) goto end;
+
+	// Interacción 15
+	respuestaActual = CrearMostrarQueueInteraccion(interaccionActual, interaccion15, respuestas);
+
+	if (respuestaActual == 1)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia1_15);
+		goto end;
+	}
+
+	if (respuestaActual == 2)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia2_15);
+	}
+
+	if (vida <= 0) goto end;
+
+	// Interacción 16
+	respuestaActual = CrearMostrarQueueInteraccion(interaccionActual, interaccion16, respuestas);
+
+	if (respuestaActual == 1)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia1_16);
+	}
+
+	if (respuestaActual == 2)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia2_16);
+		vida = vida - 30;
+	}
+
+	if (vida <= 0) goto end;
+
+	// Interacción 17
+	respuestaActual = CrearMostrarQueueInteraccion(interaccionActual, interaccion17, respuestas);
+
+	if (respuestaActual == 1)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia1_17);
+	}
+
+	if (respuestaActual == 2)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia2_17);
+		vida = vida - 20;
+	}
+
+	if (vida <= 0) goto end;
+
+	// Interacción 18
+	respuestaActual = CrearMostrarQueueInteraccion(interaccionActual, interaccion18, respuestas);
+
+	if (respuestaActual == 1)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia1_18);
+		goto end;
+	}
+
+	if (respuestaActual == 2)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia2_18);
+	}
+
+	if (vida <= 0) goto end;
+
+	// Interacción 19
+	respuestaActual = CrearMostrarQueueInteraccion(interaccionActual, interaccion19, respuestas);
+
+	if (respuestaActual == 1)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia1_19);
+	}
+
+	if (respuestaActual == 2)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia2_19);
+		goto end;
+	}
+
+	if (vida <= 0) goto end;
+
+	// Interacción 20
+	respuestaActual = CrearMostrarQueueInteraccion(interaccionActual, interaccion20, respuestas);
+
+	if (respuestaActual == 1)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia1_20);
+		goto win;
+	}
+
+	if (respuestaActual == 2)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia2_20);
+		goto win;
+	}
+
+	if (vida <= 0) goto end;
+
+torito:
+
+	// Interacción 11
+	respuestaActual = CrearMostrarQueueInteraccion(interaccionActual, interaccion11, respuestas);
+
+	if (respuestaActual == 1)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia1_11);
+	}
+
+	if (respuestaActual == 2)
+	{
+		CrearMostrarConsecuencia(consecuenciaActual, consecuencia2_11);
+	}
+
+
+end:
+
+	std::cout << "Perdiste ¡Gracias por jugar!";
+
+win:
+
+	std::cout << "Ganaste ¡Felicidades!";
+
+	delete interaccionActual;
+	delete consecuenciaActual;
 }
---
+++ Text.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <string>;
+
+// Texto de las decisiones
+
+std::string interaccion1 = "¡Bienvenido a mi juego! \n\nToma las decisiones correctas para triunfar y salir victorioso en esta historia.\n\nPrimero que nada, ¿Cuál es tu nombre?";
+std::string interaccion2 = "Despierta… por favor, es muy importante que votes por que- ”\n\nAntes de que la misteriosa voz pueda completar su oración, suena tu alarma.Miras la fecha, es 10 de abril, día de la votación por la revocación de mandato.\n\n\n1.No me importa, tengo sueño y me vuelvo a dormir.\n2.Tengo que alistarme para salir a votar.";
+std::string interaccion3 = "Justo antes de vestirte, sientes que vas a estornudar.\n\n1. Estornudo\n2. Me aguanto el estornudo. ";
+std::string interaccion4 = "Te pones tu playera fachera facherita y tus pantalones llenos de swag. Solo te falta abrocharte las agujetas.\n\n1. Te abrochas las agujetas.\n2. No te abrochas las agujetas.";
+std::string interaccion5 = "Te sientas por un momento en tu cama y tienes la tentación de checar Twitter.\n\n1. Checas Twitter\n2. No checas twitter.";
+std::string interaccion6 = "Bajas a desayunar. Checas el refrigerador y ves que hay un ceviche en un topper, pero no recuerdas de cuándo es.\n\n1. Te comes el ceviche.\n2. Te haces unos huevitos con jamón.";
+std::string interaccion7 = "Bajas las escaleras y ves al gato acostado en el sillón. Parece estar profúndamente dormido.\n\n1. Lo acaricias.\n2. No lo acaricias";
+std::string interaccion8 = "Te acuerdas de que no has hecho tu declaración de impuestos y hoy es la fecha límite.\n\n1. Los declaras de manera honesta.\n2. Evades impuestos";
+std::string interaccion9 = "Ya estás listo para votar, pero escuchas que tu mamá te dice “Lava los trastes antes de irte”.\n\n1. Le dices que los lavarás al volver.\n2. Los lavas en el momento.";
+std::string interaccion10 = "Ya estás listo para salir a votar, pero de repente, recuerdas que tienes una botella de Kosako en el refri. Aunque la casilla está a 5 minutos caminando, piensas ir en coche.\n\n1. No tomas alcohol como persona responsable.\n2. Tomas Kosako.";
+std::string interaccion11 = "Tomas un vaso entero de Kosako y te subes a tu coche. Justo saliendo del fraccionamiento, un policía te pide que pases a hacer una prueba en el alcoholímetro. Como acabas de tomar Kosako, tu nivel de alcohol sale el doble de lo permitido para manejar.\n\n1. Ni modo, te vas al torito.\n2. Sobornas al policía.";
+std::string interaccion12 = "Llegando al lugar de la casilla te das cuenta que todos los lugares cerca están ocupados, excepto dos al otro lado de la calle:\n\n1. Te estacionas en el lugar de adelante.\n2. Te estaciones en el lugar de atrás, que se encuentra debajo de un piano colgando de una cuerda muy delgada.";
+std::string interaccion13 = "Abres la puerta de tu coche y decides salir.\n\n1. Miras al suelo antes de bajar.\n2. Solo bajas sin fijarte.";
+std::string interaccion14 = "Al bajarte del coche, notas que hay un código QR pegado a la pared de la calle con una nota que dice: “CUIDADO: Demasiado Chistoso”.\n\n1. Escaneas el código QR.\n2. Lo ignoras";
+std::string interaccion15 = "Las casillas para votar se encuentran del otro lado de la calle, por lo que tienes que cruzar para llegar.\n\n1. Solo te cruzas sin mirar a los lados.\n2. Miras a los lados antes de cruzar.";
+std::string interaccion16 = "Casi llegando a la fila para votar, ves que hay un grupo de chairos coreando “Es un honor estar con obrador”. \n\n1. Los ignoras completamente.\n2. Les haces un comentario negativo sobre el presidente.";
+std::string interaccion17 = "Ya estando formado en la fila, un vecino que acaba de votar se te dirige y te saluda amablemente.\n\n1. Lo saludas de vuelta.\n2. Le haces un chiste sobre su esposa.";
+std::string interaccion18 = "En lo que estás formado llega un tipo con camisa del PRI y te dice. “Vaya, es un día muy soleado, ¿No te interesaría vender tu voto por este lindo paraguas?”.\n\n1. Lo tomas, ¡Es una ganga!\n2. No lo tomas";
+std::string interaccion19 = "Avanzas en la fila y estás a punto de pasar cuando te llega la duda… ¿Realmente quiero votar en la revocación de mandato?\n\n1. Sí, quiero votar.\n2. No, no quiero votar.";
+std::string interaccion20 = "Ya estás en la casilla de votación, y la pregunta es la siguiente: “¿Estás de acuerdo en que a Andrés Manuel López Obrador, Presidente de los Estados Unidos Mexicanos, se le revoque el mandato por pérdida de la confianza o siga en la Presidencia de la República hasta que termine su periodo?”.\n\n1. Que se le revoque el mandato por pérdida de confianza.\n2. Que siga en la Presidencia de la República.";
+
+// Texto de las consecuencias
+
+std::string consecuencia1_2 = "Te quedaste dormido y no votaste por nadie. La elección se quedó a un voto de ser valida.";
+std::string consecuencia1_3 = "Estornudas como cualquier persona normal.";
+std::string consecuencia1_4 = "Te abrochas las agujetas para no tropezar";
+std::string consecuencia1_5 = "¿Qué esperabas? Toda la gente se está peleando entre sí tratando de verse superior a los demás. Sientes como tu alma se quiere salir de tu cuerpo";
+std::string consecuencia1_6 = "El ceviche no olía mal, entonces decides comerlo. Casi inmediatamente sientes algo en el estómago y vas al baño. Crees que no debiste comerlo.";
+std::string consecuencia1_7 = "Acaricias al gato pensando que le iba a gustar, pero te araña al momento de tocarlo.";
+std::string consecuencia1_8 = "Sabes que no van a hacer nada productivo con tu dinero, pero es mejor eso a vivir en la carcel.";
+std::string consecuencia1_9 = "Tu madre, enojada, te avienta una chancla que te pega justo en la cara.";
+std::string consecuencia1_10 = "Obviamente no tomas Kosako justo antes de manejar, sería bastante irresponsable.";
+std::string consecuencia1_11 = "Te llevan al torito y no puedes votar en la revocación de mandato.";
+std::string consecuencia1_12 = "“¿Por qué rayos me estacionaría debajo de eso? ¡Sería una muerte demasiado cómica!” Piensas. Te estaciones de manera segura en el lugar de adelante.";
+std::string consecuencia1_13 = "Al fijarte antes de bajar, te percatas de que hay una popó de perro justo al lado de la puerta y logras evitar pisarla.";
+std::string consecuencia1_14 = "Al escanear el código ves que tan solo contiene una palabra: “Piripitifláuica”. Al leerlo, inmediatamente mueres de la risa… literalmente.";
+std::string consecuencia1_15 = "Estás cruzando la calle cuando de repente llega un coche negro a toda velocidad y te atropella. Caes al suelo inmediatamente y en tus últimos momentos puedes ver como sale un hombre del coche y te dice “Tuve fé. Familia”. Mueres justo después.";
+std::string consecuencia1_16 = "Sabes que no tiene ningún punto hablar de política en público y procedes a formarte.";
+std::string consecuencia1_17 = "Aunque te dan ganas de hacer un chiste, sientes que algo malo pasaría si lo hicieras, por lo que simplemente lo saludas y sigues en la fila.";
+std::string consecuencia1_18 = "Pff, ¡¿Es en serio, un paraguas por tu voto?! ¡Claro que lo tomas!";
+std::string consecuencia1_19 = "Ok, lo entiendo, quieres votar.";
+std::string consecuencia1_20 = "Votas y cuando dan los resultados revelan que Andrés Manuel será revocado de su mandato, perdiendo por tan solo un voto.";
+
+std::string consecuencia2_2 = "Sabes que es muy importante votar, por lo que decides ir a una casilla.";
+std::string consecuencia2_3 = "Te aguantas el estornudo y terminas explotando.";
+std::string consecuencia2_4 = "No te abrochas las agujetas y tropiezas inmediatamente. Justo después de levantarte, te las abrochas aún así";
+std::string consecuencia2_5 = "Hiciste bien… hiciste bien";
+std::string consecuencia2_6 = "Con flojera y de mala gana te haces unos huevitos con jamón. No será tan fácil como comer el ceviche, pero al menos sabes que es seguro.";
+std::string consecuencia2_7 = "Decides no acariciarlo. Nunca sabes lo que pasa por su mente.";
+std::string consecuencia2_8 = "¡Te vale un pepino! Evades impuestos. Inmediatamente escuchas que tocan la puerta. Es el SAT. Estás frito.";
+std::string consecuencia2_9 = "Ni modo, te toca lavar trastes. Es muy aburrido y monótono, pero lo haces.";
+//La consecuencia2_10 = Interaccion11
+std::string consecuencia2_11 = "El policía no acepta el soborno y te mete a la carcel.";
+std::string consecuencia2_12 = "“¿Qué es lo peor que puede pasar?” Piensas. Justo cuando abres la puerta después de estacionarte, el piano cae de la cuerda, aplastándote a muerte junto a tu coche.";
+std::string consecuencia2_13 = "Como no te fijas antes de bajarte, terminas pisando una popó de perro que se encontraba en el pastito de la banqueta.";
+std::string consecuencia2_14 = "“Hmm, no ha de ser tan gracioso.”";
+std::string consecuencia2_15 = "Logras darte cuenta de que a toda velocidad viene un 1970 Dodge Charger R/T siendo manejado por un tipo pelón. Pasas al otro lado de forma segura.";
+std::string consecuencia2_16 = "Ni si quiera alcanzas a terminar de decir una oración y el grupo de chairos van a gritarte y amontonarte por unos segundos. Cuando por fin te dejan en paz, procedes a formarte ";
+std::string consecuencia2_17 = "Haces un chiste sobre la alopecia de su esposa, pensando que al menos le daría risa. Inicialmente así es, pero después se enfurece, se te acerca y te da una bofetada tremenda. “Mantén el nombre de mi esposa fuera de tu hermosa boca” te dice. Después de esto, se va y te quedas en la fila.";
+std::string consecuencia2_18 = "Obvio no lo voy a hacer, eso iría en contra de la democracia.";
+std::string consecuencia2_19 = "Decides que no vas a votar y regresas a tu casa. Cuando por fin dan los resultados, revelan que la votación se quedó a un voto de ser valida.";
+std::string consecuencia2_20 = "Votas y cuando dan los resultados revelan que Andrés Manuel seguirá siendo presidente, ganando por tan solo un voto.";
+
+
